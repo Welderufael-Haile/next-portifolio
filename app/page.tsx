d3a98178cfaec614da395b77c0c2343a7e2a1f22
welderufaelhaile <rufaelhaile14@gmail.com>
2025-04-07 13:53:06 +0300

    update root page.js

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 
+import About from "./pages/about/About";
 import HomePage from "./pages/home/Home";
 export default function Home() {
   return (
     <div className="h-full mt-10">
       <HomePage></HomePage>
+      <About></About>
     </div>
   );
 }
