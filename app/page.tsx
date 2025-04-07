b5d439a71b1c0c8f41708c92887423f6add80dbd
welderufaelhaile <rufaelhaile14@gmail.com>
2025-04-07 14:27:13 +0300

    update root page.js

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,15 @@
 
 import About from "./pages/about/About";
 import HomePage from "./pages/home/Home";
+import Contact from "./pages/contact/Contact";
+import Skills from "./pages/skills/Skills";
 export default function Home() {
   return (
     <div className="h-full mt-10">
       <HomePage></HomePage>
       <About></About>
+      <Skills></Skills>
+      <Contact></Contact>
     </div>
   );
 }
